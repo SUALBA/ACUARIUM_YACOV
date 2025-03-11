f5b458147c52023bcb828356c726f920e2aa25b5
SUALBA <112857703+SUALBA@users.noreply.github.com>
2025-03-11 04:45:05 +0100

    Update firebaseConfig.js
    borre datos de acceso a firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,15 +3,6 @@ import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAPU11y6y0UxJezD0fm0qz_RkEL-P9PVZk",
-  authDomain: "acuarium-yacov.firebaseapp.com",
-  projectId: "acuarium-yacov",
-  storageBucket: "acuarium-yacov.firebasestorage.app",
-  messagingSenderId: "516263497960",
-  appId: "1:516263497960:web:d61d3617f923b399c1b95d",
-  measurementId: "G-P0PQMV7MXF"
-};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
